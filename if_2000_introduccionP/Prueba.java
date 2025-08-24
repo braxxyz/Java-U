9b8843226855b21695d8f4525242e0d12fe6e02f
braxxyz <yenamoretty2327@gmail.com>
2025-08-24 16:45:55 -0600

    algoritmot con un cuadrado

--- if_2000_introduccionP/Prueba.java
+++ if_2000_introduccionP/Prueba.java
@@ -1,6 +1,9 @@
 public class Prueba {
     public static void main(String[] args) {
        
+int 
+
+
         System.out.println("La resta de c y a es: ");
     }
 }
---
+++ package/lab1_ejercicios.java
@@ -0,0 +1,22 @@
+public class lab1_ejercicios {
+    public static void main (String[] args) {
+
+    //esta es la parte de arriba 
+
+    System.out.println("* * * * * ");
+    System.out.println("*       * ");
+    System.out.println("*       * ");
+    System.out.println("*       * ");
+    System.out.println("*       * ");
+    System.out.println("*       * ");
+     //esta es la parte de abajo
+
+     System.out.println("* * * * * "); 
+
+
+
+
+     
+
+    }
+}
