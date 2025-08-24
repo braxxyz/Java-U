2222ea0fa7795af77ed78c908f2a4335cadfeb35
braxxyz <yenamoretty2327@gmail.com>
2025-08-24 16:53:47 -0600

    no sirve

--- package/lab1_ejercicios.java
+++ package/lab1_ejercicios.java
@@ -1,22 +1,2 @@
-public class lab1_ejercicios {
-    public static void main (String[] args) {
-
-    //esta es la parte de arriba 
-
-    System.out.println("* * * * * ");
-    System.out.println("*       * ");
-    System.out.println("*       * ");
-    System.out.println("*       * ");
-    System.out.println("*       * ");
-    System.out.println("*       * ");
-     //esta es la parte de abajo
-
-     System.out.println("* * * * * "); 
-
-
-
-
-     
-
-    }
-}
+ esta madre no lo van a validar
+// --- IGNORE ---
